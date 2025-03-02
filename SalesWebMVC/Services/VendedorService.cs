356845b373c52488fea6a74016c3f81d2ea08ab7
Thiago Anderson <thiago.andersom.silva@hotmail.com>
2025-03-01 21:48:05 -0300

    Corrijindo problema de chave estrageira(temporario)

--- SalesWebMVC/Services/VendedorService.cs
+++ SalesWebMVC/Services/VendedorService.cs
@@ -18,6 +18,7 @@ namespace SalesWebMVC.Services
         }
         public void Insert(Vendedor obj)
         {
+            obj.Departamento = _context.Departamento.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
